182c887170e9bb34f23c00be1e4f6021abbe188d
Mohit <mohit@signitysolutions.in>
2018-01-12 13:09:24 +0530

    10 commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home pagess. */
+
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/testing45', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
